841829d1c26b1bb517dbc0151fd12ff3c80ff488
Nicolas Valdez <nicolasvaldez125@gmail.comgit config --global user.name Nicolas>
2026-06-08 11:53:39 -0500

    add action screenshot, start on model.py

--- ml/model.py
+++ ml/model.py
@@ -2,6 +2,7 @@ import pickle
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
 # TODO: add necessary import
+import mlflow.sklearn
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
@@ -60,6 +61,8 @@ def inference(model, X):
         Predictions from the model.
     """
     # TODO: implement the function
+    preds = model.predict(X)
+    return preds
     pass
 
 def save_model(model, path):
@@ -73,11 +76,14 @@ def save_model(model, path):
         Path to save pickle file.
     """
     # TODO: implement the function
+    mlflow.sklearn.save_model(model, path)
     pass
 
 def load_model(path):
     """ Loads pickle file from `path` and returns it."""
     # TODO: implement the function
+    model=mlflow.sklearn.load_model(path)
+    return model
     pass
 
 
@@ -122,6 +128,9 @@ def performance_on_categorical_slice(
         # your code here
         # for input data, use data in column given as "column_name", with the slice_value 
         # use training = False
+        data_slice = data[data[column_name] == slice_value]
+        data_slice_filtered = data_slice[categorical_features]
+        data_encoded = encoder.transform()
     )
     preds = None # your code here to get prediction on X_slice using the inference function
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
